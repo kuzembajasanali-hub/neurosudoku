d21052b512f7f5003366252aadb25881af61a4f1
kuzembajasanali-hub <kuzembajasanali@gmail.com>
2026-05-30 00:35:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const PORT = Number(process.env.PORT || 5174);
-const HOST = process.env.HOST || "127.0.0.1";
+const HOST = process.env.HOST || "0.0.0.0";
 const DB_PATH = path.join(__dirname, "db.json");
 const PUBLIC_DIR = __dirname;
 
